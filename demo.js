bf783a5169247df8dce0edfcd1ce072826ae2e3d
okearnan13 <126901221+okearnan13@users.noreply.github.com>
2024-11-25 17:21:15 +0000

    Save2

--- demo.js
+++ demo.js
@@ -8,17 +8,20 @@ export const runDemo = async (turtle, ti) => {
   };
 
   const drawStar = async (size) => {
+    await turtle.setLineWidth(10);
+    await turtle.right(18);
     for (let i = 0; i <= 4; i++){
-      
+      await turtle.forward(size);
+      await turtle.right(144);
     }
-  }
+  };
 
   let keepDrawing = true;
 
   while (keepDrawing) {
     let size = await ti.promptNumber("How big a square? (type 0 to end)");
     if (size != 0) {
-      await drawSquare(size);
+      await drawStar(size);
     } else {
       keepDrawing = false;
     }
