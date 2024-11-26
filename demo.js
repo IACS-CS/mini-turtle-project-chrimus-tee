f116ba9e9150b4a52311ce4e88e6a5f17d9abb7f
okearnan13 <126901221+okearnan13@users.noreply.github.com>
2024-11-26 18:21:37 +0000

    Save3

--- demo.js
+++ demo.js
@@ -1,29 +1,35 @@
 export const runDemo = async (turtle, ti) => {
-
-  const drawSquare = async (size) => {
-    for (let i = 0; i < 4; i++) {
-      await turtle.forward(size);
-      await turtle.right(90);
-    }
-  };
-
-  const drawStar = async (size) => {
+ 
+  const drawStar = async (size, color) => {
+    await turtle.setStrokeStyle(color); // set color
+    await turtle.setFillStyle(color); 
     await turtle.setLineWidth(10);
+    await turtle.setSpeed(4000);
+    await turtle.penUp();
+    await turtle.forward(size*.5); // setting up the starting point
     await turtle.right(18);
-    for (let i = 0; i <= 4; i++){
-      await turtle.forward(size);
+    await turtle.beginPath();
+    await turtle.penDown();
+    for (let i = 0; i <= 4; i++){ // start pattern
       await turtle.right(144);
+      await turtle.forward(size);
     }
+    await turtle.fill(color); // fill
+    await turtle.penUp();
+    await turtle.left(18); // reset angle
+    await turtle.back(size/2); //reset position(kinda)
   };
+  const drawStarQuestions = async() => {
+    let size = await ti.promptNumber("How big of a star?"); // asks size
+    await ti.output("what color would you like the star?") // asks color
+    let color = await ti.readChoice(["red", "blue", "green", "gold", "silver"]);
+    return {size, color}; // returns values for the drawStar function
+  }
 
+   
   let keepDrawing = true;
-
   while (keepDrawing) {
-    let size = await ti.promptNumber("How big a square? (type 0 to end)");
-    if (size != 0) {
-      await drawStar(size);
-    } else {
-      keepDrawing = false;
-    }
+    const { size, color } = await drawStarQuestions();
+    await drawStar(size, color);
   }
 }
