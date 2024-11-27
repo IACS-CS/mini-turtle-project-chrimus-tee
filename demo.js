35254c3a5ead06ac1f589504cf263e6fa00857df
okearnan13 <126901221+okearnan13@users.noreply.github.com>
2024-11-27 14:22:59 +0000

    save4

--- demo.js
+++ demo.js
@@ -1,5 +1,10 @@
 export const runDemo = async (turtle, ti) => {
- 
+
+  const greenOrnament = ['green','gold'];
+  const redOrnament = ['red','gold'];
+  const blueOrnament = ['blue','silver'];
+  const yellowOrnament = ['gold','silver'];
+
   const drawStar = async (size, color) => {
     await turtle.setStrokeStyle(color); // set color
     await turtle.setFillStyle(color); 
@@ -25,11 +30,41 @@ export const runDemo = async (turtle, ti) => {
     let color = await ti.readChoice(["red", "blue", "green", "gold", "silver"]);
     return {size, color}; // returns values for the drawStar function
   }
+  const drawOrnament = async (size, color) => {
+    await turtle.setStrokeStyle(color.at(0));
+    await turtle.setFillStyle(color.at(0));
+    await turtle.setLineWidth(1);
+    await turtle.setSpeed(200);
+    await turtle.beginPath();
+    await turtle.left(90);
+    await turtle.arc(size, 360, true);
+    await turtle.fill(); 
+    await turtle.closePath();//end of ornament body
+    
+    await turtle.beginPath();
+    await turtle.setStrokeStyle(color.at(1));
+    await turtle.setFillStyle(color.at(1));
+    await turtle.forward(size * 0.25);
+    for (let i = 0; i <= 2; i++) {
+      await turtle.right(90);
+      await turtle.forward(size*.4);
+    }
+    
+    await turtle.right(90);
+    await turtle.forward(size*.25);
+    await turtle.fill();
+  };
+  const drawOrnamentQuestions = async () => {
+    let size = await ti.promptNumber("How big of ornaments?"); // asks size
+    await ti.output("what color would you like the ornaments?"); // asks color
+    let color = await ti.readChoice(["redOrnament", "blueOrnament", "greenOrnament", "yellowOrnament",]);
+    return {size, redOrnament}; // returns values for the drawOrnament function
+  };
 
-   
+ 
   let keepDrawing = true;
   while (keepDrawing) {
-    const { size, color } = await drawStarQuestions();
-    await drawStar(size, color);
+    const { size, color } = await drawOrnamentQuestions();
+    await drawOrnament(size, color);
   }
 }
